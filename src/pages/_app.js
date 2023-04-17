45d1403a8b87cefe0e69825af54c902dc1583933
Colby Fayock <fayock@gmail.com>
2023-04-17 16:28:28 -0400

    adding GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import { Source_Sans_Pro } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import * as gtag from '@/lib/gtag';
 
 import '@/styles/globals.scss';
 
@@ -10,6 +15,16 @@ const sourceSansPro = Source_Sans_Pro({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => gtag.pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <style jsx global>{`
@@ -17,6 +32,24 @@ export default function App({ Component, pageProps }) {
           font-family: ${sourceSansPro.style.fontFamily};
         }
       `}</style>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Component {...pageProps} />
       <Analytics />
     </>
