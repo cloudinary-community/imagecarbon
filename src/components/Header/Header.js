70b33ae00c5ab83e319bfb01ae7a0097bc7786e4
Colby Fayock <fayock@gmail.com>
2023-04-17 10:03:01 -0400

    header link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
         </p>
         <ul className={styles.headerLinks}>
           <li>
-            <a href="https://github.com/colbyfayock" rel="noreferrer">
+            <a href="https://github.com/colbyfayock/imagecarbon" rel="noreferrer">
               <FaGithub />
             </a>
           </li>
