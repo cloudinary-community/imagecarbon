3a25aa6da1b5b5feca6b18d7403bae59e9fd9b37
Colby Fayock <fayock@gmail.com>
2023-04-19 08:47:21 -0400

    gtag id

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+export const GA_TRACKING_ID = process.env.NODE_ENV !== 'development' && 'G-9LH8W25SXT';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
