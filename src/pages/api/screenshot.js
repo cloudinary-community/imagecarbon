7fdd017ee443174c1d61ec45a83102a16f58ce42
Colby Fayock <fayock@gmail.com>
2024-05-31 21:12:11 -0400

    puppeeteer

--- src/pages/api/screenshot.js
+++ src/pages/api/screenshot.js
@@ -1,27 +1,39 @@
-import captureWebsite from 'capture-website';
-import chromium from "chrome-aws-lambda";
+const chromium = require('@sparticuz/chromium-min');
+const puppeteer = require('puppeteer-core');
 
 export default async function handler(req, res) {
   try {
+    await chromium.font(
+      "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+    );
 
-    const test = await captureWebsite.buffer('https://sindresorhus.com', {
-      launchOptions: {
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless
-      }
+    const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+    const browser = await puppeteer.launch({
+      args: isLocal ? puppeteer.defaultArgs() : [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar'),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
     });
-console.log('test', test)
+
+    const page = await browser.newPage();
+console.log('new page')
+    await page.goto('https://spacejelly.dev/');
+console.log('asdf')
+    const title = await page.title();
+
+    console.log('title', title)
+    
+    await browser.close();
+
     res.status(200).json({
-      test
+      title
     });
-
   } catch(e) {
-    console.log('e', e)
-
+    console.log(`Failed to get screenshot: ${e.message}`);
     res.status(500).json({
-      e
-    });
+      error: e.message
+    })
   }
 }
